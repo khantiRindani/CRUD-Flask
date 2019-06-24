9abcb1d85b0c38d29f73d68b7f7224a6b2f8720c
Khanti Rindani <krindani99@gmail.com>
2019-06-24 18:52:00 +0000

    Static data linkage

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from data import Blogs
 
 app = Flask(__name__)
 
@@ -6,5 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/blogs')
+def blogs():
+    return render_template('blogs.html',blogs=Blogs())
+
 if __name__=='__main__':
     app.run(debug='true')
+
---
+++ data.py
@@ -0,0 +1,22 @@
+def Blogs():
+
+    blogs=[
+        {'id': 'blog1',
+         'title': 'blog one',
+         'author': 'khanti rindani',
+         'content': 'some dummy text',
+         'creation_date': '03-06-2018'},
+
+         {'id': 'blog2',
+         'title': 'blog two',
+         'author': 'piri rindani',
+         'content': 'some dummy text',
+         'creation_date': '28-02-2018'
+         },
+
+         {'id': 'blog3',
+          'title': 'blog three',
+          'author': 'khanti rindani',
+          'content': 'some dummy text',
+          'creation_date': '03-06-2018'}]
+    return blogs
